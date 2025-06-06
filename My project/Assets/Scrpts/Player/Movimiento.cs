4c25b6048e45128278dad74c4909202f02a9946c
CrazyWitch01 <ignaciopenasumari@gmail.com>
2025-06-06 16:44:51 -0500

    DEBUG2

---
+++ My project/Assets/Scrpts/Player/Movimiento.cs
@@ -0,0 +1,106 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Movimiento : MonoBehaviour
+{
+    [SerializeField] float velocidad = 5f;
+    public Transform Orientation;
+    public float PlayerHeight;
+    public LayerMask Ground;
+    public bool grounded;
+    public float groundDrag;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    public bool canJump;
+    private KeyCode jumpKey = KeyCode.Space;
+
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        canJump = true;
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void InputFunc()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpKey) && canJump && grounded )
+        {
+            canJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rayOrigin = transform.position + Vector3.up * 1f;
+        float rayLength = PlayerHeight * 0.5f + 0.2f;
+        Debug.DrawRay(rayOrigin, Vector3.down * rayLength, Color.red);
+        grounded = Physics.Raycast(rayOrigin, Vector3.down, rayLength, Ground);
+        InputFunc();
+        SpeedControl();
+
+        if (grounded)
+        {
+            rb.linearDamping = groundDrag;
+        }
+        else
+        {
+            rb.linearDamping = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    private void MovePlayer()
+    {
+        moveDirection = Orientation.forward*verticalInput + Orientation.right*horizontalInput;
+
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * velocidad * 10f, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * velocidad * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+    
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x,0f,rb.linearVelocity.z);
+        if(flatVel.magnitude>velocidad)
+        {
+            Vector3 limitedvel = flatVel.normalized * velocidad;
+            rb.linearVelocity = new Vector3(limitedvel.x, rb.linearVelocity.y, limitedvel.z);
+        }
+    }
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x,0,rb.linearVelocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        canJump = true;
+    }
+}
